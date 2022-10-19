ab204e8d4b7a5dc2ae00fcf907a0146a5b5223ba
banggeunho <panggeunho@gmail.com>
2022-10-19 23:45:40 +0900

    change name

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,17 +1,15 @@
 # 가동중인 awsstudy 도커 중단 및 삭제
 #sudo docker ps -a -q --filter "name=formduo" | grep -q . && docker stop formduo && docker rm formduo | true
 
-sudo bash
+sudo docker-compose stop
 
-docker-compose stop
-
-docker-compose rm -y
+sudo docker-compose rm
 
 # 기존 이미지 삭제
-docker rmi qkdrmsgh73/formduo
+sudo docker rmi qkdrmsgh73/formduo
 
 # 도커 실행
-docker-compose up
+sudo docker-compose up
 
 ## 도커허브 이미지 pull
 #sudo docker pull qkdrmsgh73/formduo
@@ -20,4 +18,4 @@ docker-compose up
 #docker run -d -p 8082:8080 --name formduo qkdrmsgh73/formduo
 
 # 사용하지 않는 불필요한 이미지 삭제 -> 현재 컨테이너가 물고 있는 이미지는 삭제되지 않습니다.
-docker rmi -f $(docker images -f "dangling=true" -q) || true
+sudo docker rmi -f $(docker images -f "dangling=true" -q) || true
