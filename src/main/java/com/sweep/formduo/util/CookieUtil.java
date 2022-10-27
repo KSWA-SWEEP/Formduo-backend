cf2eefc0099a41823e3f6ba6435df735efbf5fb3
banggeunho <panggeunho@gmail.com>
2022-10-27 15:20:06 +0900

    [SWEEP98] 쿠키 수정

--- src/main/java/com/sweep/formduo/util/CookieUtil.java
+++ src/main/java/com/sweep/formduo/util/CookieUtil.java
@@ -30,7 +30,7 @@ public class CookieUtil {
     public static void addCookie(HttpServletResponse response, String name, String value, int maxAge) {
         Cookie cookie = new Cookie(name, value);
         cookie.setPath("/");
-        cookie.setHttpOnly(true);
+        cookie.setHttpOnly(false);
         cookie.setMaxAge(maxAge);
 
         response.addCookie(cookie);
